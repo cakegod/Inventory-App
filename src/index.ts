8fe2d2b40a2feb413aafe7714efc6ed62345e5cf
Fabio <36206278+cakegod@users.noreply.github.com>
2022-09-30 23:31:12 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import debug from 'debug';
+import http from 'http';
+import { app } from './app';
+
+// error handler
+const onError = (error: NodeJS.ErrnoException) => {
+	if (error.syscall !== 'listen') throw error;
+	switch (error.code) {
+		case 'EACCES':
+			console.error(`Port ${port} requires elevated privileges.`);
+			process.exit(1);
+		case 'EADDRINUSE':
+			console.error(`Port ${port} is already in use.`);
+			process.exit(1);
+		default:
+			throw error;
+	}
+};
+
+// event listener
+const onListening = () => {
+	const address = server.address();
+	const bind =
+		typeof address === 'string'
+			? `pipe ${address}`
+			: `port ${address?.port}`;
+	debug(`Listening on ${bind}`);
+};
+
+const port: Number = +process.env.PORT! || 3000;
+const server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
