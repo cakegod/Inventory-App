8be470f26fbe4bd2459700c7159b2f07c0a13f0c
Cake <cakemancerr@gmail.com>
2022-10-02 21:38:10 +0200

    Add routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import categoryController from '../controllers/categoryController';
 import productController from '../controllers/productController';
 
 const router = express.Router();
 
-router.route('/').get(productController.index);
+router.get('/', productController.index);
+router.get('/products', productController.displayProductsList);
+router.get('/product/:id', productController.displayProductDetails);
+router.get('/category/:id', categoryController.displayCategoryProducts);
 
 export default router;
